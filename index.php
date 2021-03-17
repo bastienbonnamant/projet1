c63abe301d165b7d75cc52824ea4e45fcaa8b533
Klipfel-Nicolas <klipfelnicolas86@gmail.com>
2021-03-17 16:07:40 +0100

    brouillon

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link href="/index.css" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jacques Chirac</title>
+</head>
+<body>
+    
+    
+    <section class="home">
+        <?php include "menu.php" ?>
+        <main>
+            <article>
+                <p>Bonjour, je m'appelle</p>
+                <h1>Chirac, Jacques Chirac</h1>
+                <p>et je suis un mangeur de <span>Pomme</span></p>
+            </article>
+
+            <aside>
+                <img src="/img/Chirac_pose.png" alt="Chirac fume"/>
+            </aside>
+        </main>
+    </section>
+
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,37 @@
+<header>
+    <div class="container">
+        <div class="logo">
+            <img src="/img/chirac_logo_2.png" alt="logo jacques chirac" />
+        </div>
+
+        <div class="photo">
+            <img src="/img/chiracAirline.png" src="chirac dort dans l'avion"/>
+        </div>
+
+        <h2>
+            Président
+        </h2>
+
+        <nav>
+            <ul>
+                <li><a>Biographie</a></li>
+                <li><a>Réplique</a></li>
+                <li><a>Présidence</a></li>
+                <li><a>Guignols</a></li>
+                <li><a>Culture Pop</a></li>
+            </ul>
+        </nav>
+
+        <div class="quote">
+            " Citation aléatoire "
+        </div>
+
+        <div class="socialNet">
+            <ul>
+                <li><a href="https://www.facebook.com/jacques.chirac.921" target="blank"><img src="/img/facebook.png" alt="Logo lien facebook"></a></li>
+                <li><a href="https://www.pinterest.fr/search/pins/?q=chirac&rs=typed&term_meta[]=chirac%7Ctyped" target="blank"><img src="/img/pinterest.png" alt="Logo lien Pinterest"/></a></li>
+                <li><a href="https://www.instagram.com/fuckyeahjacqueschirac/?hl=en" target="blank"><img src="/img/instagram.png" alt="Logo lien Instagram"/></a></li>
+            </ul>
+        </div>
+    </div>
+</header>
