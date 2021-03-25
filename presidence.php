d011858d46edfa08afea4ce038bdeb34b7dce755
Maxime <maximej.becker@gmail.com>
2021-03-25 15:14:12 +0100

    enhance

--- presidence.php
+++ presidence.php
@@ -25,18 +25,54 @@
              <p><br><br><br></p>
 
              <img class="pics" src="img/DTywGGpWkAExYMF.jpeg">
-            <p> Quatrième président de la Vème République </p><br>
+            <h4 class="intertitre"><br> Quatrième président de la Vème République <br> </h4>
 
-    <p>Il était arrivé en deuxième position au premier tour de l'élection présidentielle, le 23 avril 1995, avec 20,8% des voix,
+    <p><br>Il était arrivé en deuxième position au premier tour de l'élection présidentielle, le 23 avril 1995, avec 20,8% des voix,
      derrière Lionel Jospin (23,3%). Mais au second, Jacques Chirac s'impose largement face au candidat PS, avec 52,6% des suffrages.
       Il rompt ainsi avec les deux septennats socialistes de François Mitterrand.<br>
 
        <br>Le soir de sa victoire, Jacques Chirac attend les résultats dans son bureau, à l'hôtel de ville de Paris. Il est maire de la capitale depuis 1977.
         Il prend la parole depuis un salon de l'édifice. Son discours est retransmis en direct sur un écran géant installé place de la Concorde.<br><br> </p>
-    
-    <p> Durant son premier septennat, Chirac a instauré, entre autre, le quiquenat présidentielle, dont il profitera en 2002, lors de sa réelection.</p>
 
+        <section class="trait"></section>
 
+        <h4 class ="intertitre"> Les mesures phares </h4>
+
+        <img class="pics" src="img/jacques_chirac.jpg">
+       <h5 class="titrepetit"> Le quiquenat </h5>
+    <p><br> Durant son premier septennat, Chirac a instauré, entre autre, le quiquenat présidentielle, dont il profitera en 2002, lors de sa réelection.<br></p>
+        <h5 class="titrepetit"><br> Lutte contre le cancer </h5>
+    <p><br>Le 14 juillet 2002, lors du traditionnel discours de la Fête nationale,
+     Jacques Chirac fait de la lutte contre le cancer une priorité nationale.
+      Un discours suivi d’effets, jusqu’à l’entrée en vigueur d’une loi phare,
+       le 1er janvier 2008 : après des semaines de débats, c’est sous sa présidence qu’entre en vigueur l’interdiction de fumer dans les lieux publics fermés,
+        comme les restaurants, les cafés, ou les boîtes de nuit. Une mesure qui fait grincer des dents au début, mais qui, mois après mois,
+         entre dans les habitudes des Français.<p>
+
+<p><br>Cette loi, c’est la mesure phare du président Chirac, et de son plan de santé publique pour lutter contre le cancer.
+ Un plan, qui a fait des émules puisqu’on en est aujourd’hui au troisième plan contre le cancer.<br></p>
+        <h5 class="titrepetit"><br> Fin du service militaire obligatoire </h5>
+ <p><br>Autre décision de Jacques Chirac qui marque la vie des Français, des jeunes en particulier,
+      sa décision de professionnaliser l’armée française et au passage de suspendre le service militaire obligatoire,
+       remplacé par une Journée d’appel et de Préparation à la Défense, désormais appelée Journée Défense et Citoyenneté.<br>
+
+Lors de son deuxième mandat,
+ Jacques Chirac s’attaque à la sécurité routière, entre 2003 et 2007,
+  et parvient à faire diminuer de 40% le nombre de morts sur les routes. Autre loi phare de son second mandat,
+   la loi de février 2005, en faveur des personnes handicapées. Elle se traduit par la création d’une prestation compensatoire au handicap.<br></p>
+
+   <h5 class="titrepetit"><br> Rétablir la vérité de l'histoire   </h5>
+   <section class="mesures">
+   <p><br>Jacques Chirac veut également rétablir la vérité de l’Histoire de France,
+    et prend à bras le corps la difficile question de la responsabilité de l’Etat dans la déportation de Juifs durant la Seconde guerre mondiale.
+     Le 16 juillet 1995, quelques mois à peine après son arrivée au pouvoir et à l'occasion du 53e anniversaire de la rafle du vélodrome d'hiver,
+      Jacques Chirac reconnait “la faute collective” de la France et déclare :<br>
+       “ces heures noires souillent à jamais notre histoire et sont une injure à notre passé et à nos traditions.
+        Oui, la folie criminelle de l'occupant a été secondée par des Français, par l'État français”.<br></p>
+
+     <img class="vichy" src="img/vichy.jpg">
+       </section>
+      <h4 class ="intertitre"> Jacques Chirac, réelu président des français le 5mai 2022 </h4>
     </section>
     </section>
      <script scr="/script.js">
